73ba840a865c0b6efcf5227aa02534a5c4ee0dad
Paulo Roberto Pereira <pauko.rps@gmail.com>
2025-06-26 20:24:42 -0300

    Initial commit

---
+++ src/Moedas.java
@@ -0,0 +1,5 @@
+public record Moedas(String base_code, String target_code,
+                     String conversion_rate, String conversion_result){
+// Onde: base_code= moeda principal,traget_code= moeda convertida, conversion_rate= moeda convertida, conversion_result= valor convertido da moeda
+
+}
